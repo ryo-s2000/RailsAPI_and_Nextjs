088bd966570bf9df994f64d20bd5ae05e1275dd1
Ryo Suzuki <ryo.s12330@gmail.com>
2022-09-30 18:12:37 +0900

    set format and lint

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,7 +1,33 @@
 module.exports = {
     root: true,
-    parserOptions: {
-        sourceType: "module",
-        ecmaVersion: 2015,
+    env: {
+      es2020: true,
+      node: true,
     },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      sourceType: 'module',
+      ecmaVersion: 2020,
+      tsconfigRootDir: __dirname,
+    },
+    plugins: [
+        "react",
+        "@typescript-eslint"
+    ],
+    extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+    ],
+    rules: {
+        "react/prop-types": "off",
+        "react/jsx-uses-react": "off",
+        "react/react-in-jsx-scope": "off"
+    },
+    settings: {
+        react: {
+          version: "detect"
+        }
+    }
 };
